41a894030ce896e3e4b9f39bd4381f9d613dc783
mikhail <iosmishka@gmail.com>
2020-02-19 23:02:17 +0300

    Init commit

---
+++ Tinkoff Tech Authentificator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinkoff Tech Authentificator
+//
+//  Created by Mikhail on 19.02.2020.
+//  Copyright © 2020 Tinkoff Courses. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
