12658fb5379d7c41d1cccb112255ce519eb14364
Nelmeris <kufaevartem@icloud.com>
2020-03-04 17:08:44 +0300

    [#15, #13] Auth Pin Screen example process

--- LocalPods/AuthPinScreen/Example/AuthPinScreen/ViewController.swift
+++ LocalPods/AuthPinScreen/Example/AuthPinScreen/ViewController.swift
@@ -9,23 +9,33 @@
 import UIKit
 import AuthPinScreen
 
-class ViewController: AuthPinController, AuthPinControllerDelegate {
+class ViewController: UIViewController, AuthPinControllerDelegate {
 
     let testPin = "1111"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupAuthPinController()
-        delegate = self
-//        delegate?.state = .setPin
-        delegate?.state = .confirmPin(testPin)
     }
     
     func setupAuthPinController() {
-        let authPinBundle = Bundle(for: AuthPinController.self)
-        if let authPinController = authPinBundle.loadNibNamed("AuthPinController", owner: self, options: nil)?.first as? AuthPinController {
-            navigationController?.pushViewController(authPinController, animated: true)
-        }
+//        let bundle = Bundle(for: TestViewController.self)
+        
+//        let bundle = Bundle(for: AuthPinController.self)
+//        let controller = AuthPinController(nibName: "AuthPinController", bundle: bundle)
+        
+        let controller = AuthPinController(nibName: "AuthPinController", bundle: nil)
+        
+        //1
+//        guard let controller = bundle.loadNibNamed("TestViewController", owner: nil, options: nil)?.first as? TestViewController  else { return }
+        
+        //2
+//        let controller = TestViewController(nibName: "TestViewController", bundle: bundle)
+        
+//        guard let controller = AuthPinController.new(with: self) else { return }
+//        controller.state = .setPin
+        
+        navigationController?.pushViewController(controller, animated: true)
     }
     
     //if state is setPin
@@ -36,10 +46,6 @@ class ViewController: AuthPinController, AuthPinControllerDelegate {
     //if state is confirmPin
     func didSuccessSignIn() {
         print("Success Sign in")
-    }
-    
-    func didTouchSkipBtn() {
-        print("Touch Skip Btn")
     }
     
 }
