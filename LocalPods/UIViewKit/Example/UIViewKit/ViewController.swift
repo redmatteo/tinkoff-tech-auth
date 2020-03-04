2ebbb086f252f9382629a8f155f34d4e747521f0
Nelmeris <kufaevartem@icloud.com>
2020-03-04 14:33:46 +0300

    [#15-view-kit] Example for pincode view

--- LocalPods/UIViewKit/Example/UIViewKit/ViewController.swift
+++ LocalPods/UIViewKit/Example/UIViewKit/ViewController.swift
@@ -11,6 +11,8 @@ import UIViewKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pincodeView: PincodeView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,38 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func clicked(_ sender: Any) {
+        pincodeView.fillNextDot(animated: true)
+    }
 
+    @IBAction func removed(_ sender: Any) {
+        pincodeView.emptyLastDot(animated: true)
+    }
+    
+    @IBAction func empty(_ sender: Any) {
+        pincodeView.emptyAllDots(animated: true)
+    }
+    
+    @IBAction func randomColors(_ sender: Any) {
+        pincodeView.emptyDotColor = .random()
+        pincodeView.filledDotColor = .random()
+    }
+    
 }
 
+
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+
+extension UIColor {
+    static func random() -> UIColor {
+        return UIColor(red:   .random(),
+                       green: .random(),
+                       blue:  .random(),
+                       alpha: 1.0)
+    }
+}
