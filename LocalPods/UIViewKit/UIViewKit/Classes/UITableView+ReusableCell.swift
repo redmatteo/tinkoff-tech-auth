30425176f6872cb92a0525e56b60666dd3161427
Nelmeris <kufaevartem@icloud.com>
2020-03-04 18:21:46 +0300

    [#14-table-adapter] Added Reusable Cell protocols

--- LocalPods/UIViewKit/UIViewKit/Classes/UITableView+ReusableCell.swift
+++ LocalPods/UIViewKit/UIViewKit/Classes/UITableView+ReusableCell.swift
@@ -1,9 +1,19 @@
 //
-//  UITableView+Reusable.swift
+//  UITableView+ReusableCell.swift
 //  Pods-UIViewKit_Example
 //
 //  Created by Artem Kufaev on 04.03.2020.
 //
+
+public protocol ReusableCell {
+    static var reuseIdentifier: String { get }
+}
+
+public extension ReusableCell {
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
 
 extension UITableView {
 
